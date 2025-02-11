b800b64937c06e9ab2555d95786a306dfb4261fa
ian <yihengz@outlook.com>
2025-02-12 02:10:14 +0800

    feat: 🎸 init

---
+++ README.md
@@ -0,0 +1,28 @@
+## Config files for your JS/TS projects.
+
+```bash
+bun install -D es-proj
+```
+
+### tsconfig.json
+Available extends:
+
+- `es-proj/tsconfig/dom`
+- `es-proj/tsconfig/node`
+- `es-proj/tsconfig/react`
+- `es-proj/tsconfig/rn`
+
+```json
+{
+  "extends": "es-proj/tsconfig/react",
+  "compilerOptions": {}
+}
+```
+
+### biome.json
+```json
+{
+  "$schema": "./node_modules/@biomejs/biome/configuration_schema.json",
+  "extends": ["es-proj/biome"]
+}
+```
---
+++ release.config.ts
@@ -0,0 +1,16 @@
+import type { GlobalConfig } from 'semantic-release';
+
+// https://semantic-release.gitbook.io/semantic-release/usage/configuration
+const config: GlobalConfig = {
+  repositoryUrl: 'https://github.com/ianzone/antd-enhanced',
+  tagFormat: 'v${version}',
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+};
+
+export default config;
