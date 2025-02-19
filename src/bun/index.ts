8eaf95bf551ecb772f8a96e9a6e09b86f234463c
ian <yihengz@outlook.com>
2025-02-19 21:15:39 +0800

    feat: 🎸 add trim to run()

--- src/bun/index.ts
+++ src/bun/index.ts
@@ -6,8 +6,8 @@ import { $ } from 'bun';
 export async function run(cmd: string) {
   const { stdout, stderr, exitCode } = await $`${{ raw: cmd }}`.nothrow().quiet();
   return {
-    stdout: stdout.toString(),
-    stderr: stderr.toString(),
+    stdout: stdout.toString().trim(),
+    stderr: stderr.toString().trim(),
     exitCode,
   };
 }
