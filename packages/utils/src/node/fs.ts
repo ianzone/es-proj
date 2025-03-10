75d7b14d99a19f7e559ae4c1ee0c41affaff7f00
ian <yihengz@outlook.com>
2025-03-10 16:29:59 +0800

    feat: 🎸 lookDown

--- packages/utils/src/node/fs.ts
+++ packages/utils/src/node/fs.ts
@@ -1,12 +1,46 @@
-import { existsSync } from 'node:fs';
-import { sep } from 'node:path';
+import { existsSync, statSync } from 'node:fs';
+import { join, sep } from 'node:path';
+import { glob } from 'glob';
 
-export function lookUp(fileOrFolder: string, startDir?: string): string | undefined {
-  const cwd = startDir ? startDir.split(sep) : process.cwd().split(sep);
+export function lookUp(target: string, startDir: string = process.cwd()): string | undefined {
+  const cwd = startDir.split(sep);
   for (let i = cwd.length; i > 0; i--) {
     const dir = cwd.slice(0, i).join(sep);
-    if (existsSync(`${dir}${sep}${fileOrFolder}`)) {
+    if (existsSync(`${dir}${sep}${target}`)) {
       return dir;
     }
   }
+}
+
+export function lookDown(target: string, root: string, patterns: string[]): string[] {
+  const results: string[] = [];
+  const include: string[] = [];
+  const exclude: string[] = [];
+  for (const pattern of patterns) {
+    if (pattern.startsWith('!')) {
+      exclude.push(pattern.slice(1));
+    } else {
+      include.push(pattern);
+    }
+  }
+
+  // 使用glob匹配包目录
+  const matchedPaths = glob.sync(include, {
+    ignore: exclude,
+    cwd: root,
+    absolute: true,
+  });
+
+  console.log(matchedPaths);
+  for (const matchedPath of matchedPaths) {
+    // 如果匹配到的是目录
+    if (statSync(matchedPath).isDirectory()) {
+      const targetPath = join(matchedPath, target);
+
+      if (existsSync(targetPath)) {
+        results.push(matchedPath);
+      }
+    }
+  }
+  return results;
 }
